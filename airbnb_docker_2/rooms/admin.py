51021f97b09e8dfa7094706f22fef4f64473546e
Bibi-Park <pj2sharon55@gmail.com>
2022-04-06 05:09:37 +0900

    #4 Room App Done

--- airbnb_docker_2/rooms/admin.py
+++ airbnb_docker_2/rooms/admin.py
@@ -2,6 +2,25 @@ from django.contrib import admin
 from . import models
 
 
+@admin.register(models.RoomType, models.HouseRule, models.Amenity, models.Facility)
+class ItemAdmin(admin.ModelAdmin):
+
+    """Item Admin Definition"""
+
+    pass
+
+
+@admin.register(models.Photo)
+class PhotoAdmin(admin.ModelAdmin):
+
+    """"""
+
+    pass
+
+
 @admin.register(models.Room)
 class RoomAdmin(admin.ModelAdmin):
+
+    """Room Admin Definition"""
+
     pass
--- airbnb_docker_2/rooms/models.py
+++ airbnb_docker_2/rooms/models.py
@@ -4,6 +4,71 @@ from core import models as core_models
 from users import models as user_models
 
 
+class AbstractItem(core_models.TimeStampedModel):
+
+    """Abstract Item Definition"""
+
+    name = models.CharField(max_length=80)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.name
+
+
+class Amenity(AbstractItem):
+
+    """Amenity Model Definition"""
+
+    pass
+
+    class Meta:
+        verbose_name_plural = "Amenities"
+
+
+class Facility(AbstractItem):
+
+    """Facility Model Definition"""
+
+    pass
+
+    class Meta:
+        verbose_name_plural = "Facilities"
+
+
+class HouseRule(AbstractItem):
+
+    """HouseRule Model Definition"""
+
+    pass
+
+    class Meta:
+        verbose_name = "House Rule"
+
+
+class RoomType(AbstractItem):
+
+    """RoomType Model Definition"""
+
+    pass
+
+    class Meta:
+        verbose_name = "Room Type"
+
+
+class Photo(core_models.TimeStampedModel):
+
+    """Photo Model Definition"""
+
+    caption = models.CharField(max_length=80)
+    file = models.ImageField()
+    room = models.ForeignKey("Room", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.caption
+
+
 class Room(core_models.TimeStampedModel):
 
     """Room Model Definite"""
@@ -22,3 +87,10 @@ class Room(core_models.TimeStampedModel):
     check_out = models.TimeField()
     instant_book = models.BooleanField(default=False)
     host = models.ForeignKey(user_models.User, on_delete=models.CASCADE)
+    room_type = models.ForeignKey(RoomType, on_delete=models.SET_NULL, null=True)
+    amenities = models.ManyToManyField(Amenity, blank=True)
+    facilities = models.ManyToManyField(Facility, blank=True)
+    house_rules = models.ManyToManyField(HouseRule, blank=True)
+
+    def __str__(self):
+        return self.name
