6fda745d514d3b87bc5a20d42a905a82bbb6cf7a
pj2sharon55@gmail.com <pj2sharon55@gmail.com>
2022-12-09 04:01:46 +0900

    [feat]: users views

--- airbnb_poetry/users/models.py
+++ airbnb_poetry/users/models.py
@@ -1,6 +1,3 @@
-from email.policy import default
-from secrets import choice
-from unittest.util import _MAX_LENGTH
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
