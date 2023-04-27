84eecd69281b682171fc02c4643c4c3c6f996973
pj2sharon55@gmail.com <pj2sharon55@gmail.com>
2023-04-27 14:52:52 +0900

    feat⛰: optional field & isOwner/isLiked

--- airbnb_poetry/rooms/types.py
+++ airbnb_poetry/rooms/types.py
@@ -1,10 +1,12 @@
 from django.conf import settings
 import strawberry
 from strawberry import auto
+from strawberry.types import Info
 import typing
 from . import models
 from users.types import UserType
 from reviews.types import ReviewType
+from wishlists.models import Wishlist
 
 
 @strawberry.django.type(models.Room)
@@ -15,7 +17,7 @@ class RoomType:
     owner: "UserType"
 
     @strawberry.field
-    def reviews(self, page: int) -> typing.List[ReviewType]:
+    def reviews(self, page: typing.Optional[int] = 1) -> typing.List[ReviewType]:
         page_size = settings.PAGE_SIZE
         start = page + 1
         end = start + page_size
@@ -24,3 +26,14 @@ class RoomType:
     @strawberry.field
     def rating(self) -> str:
         return self.rating()
+
+    @strawberry.field
+    def is_owner(self, info: Info) -> bool:
+        return info.context.request.user == self.owner
+
+    @strawberry.field
+    def is_liked(self, info: Info) -> bool:
+        return Wishlist.objects.filter(
+            user=info.context.request.user,
+            rooms__pk=self.pk,
+        ).exists()
