970d4c5cdf8454b21b70b265d0d87ecdb552d246
pj2sharon55@gmail.com <pj2sharon55@gmail.com>
2023-06-27 03:53:22 +0900

    fix ✅: modify amenity model

--- airbnb_poetry/rooms/models.py
+++ airbnb_poetry/rooms/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from common.models import CommonModel
+import base64
 
 
 class Room(CommonModel):
@@ -66,11 +67,26 @@ class Amenity(CommonModel):
 
     """Amenity model Definition"""
 
+    class AmenityKindChoices(models.TextChoices):
+        VIEW = "view", "전망"
+        BATH_ROOM = "bath_room", "욕실"
+        BED_ROOM_AND_LAUNDRY = "bed_room_and_laundry", "침실 및 세탁 시설"
+        ENTERTAINMENT = "entertainment", "엔터테인먼트"
+        AIR_CONDITIONING = "air_conditioning", "냉난방"
+        SAFETY = "safety", "숙소 안전"
+        WORK_SPACE = "work_space", "인터넷 및 작업 공간"
+        KICHEN_AND_DINING = "kichen_and_dining", "주방 및 식당"
+        LOCATION = "location", "위치 특성"
+        PARK_AND_ETC = "park_and_etc", "주차장 및 기타 시설"
+        SERVICE = "service", "서비스"
+
     name = models.CharField(max_length=150)
-    description = models.CharField(
-        max_length=150,
-        null=True,
+    description = models.CharField(max_length=150, blank=True, null=True)
+    kind = models.CharField(
+        choices=AmenityKindChoices.choices, max_length=100, null=True
     )
+
+    icon_image = models.TextField(null=True)
 
     def __str__(self):
         return self.name
