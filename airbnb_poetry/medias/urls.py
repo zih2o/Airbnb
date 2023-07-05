3c64f18b3785ca9a805d6cbe54b1811f7d322924
pj2sharon55@gmail.com <pj2sharon55@gmail.com>
2023-07-06 00:29:20 +0900

    feature ⛰: get upload url api

--- airbnb_poetry/medias/urls.py
+++ airbnb_poetry/medias/urls.py
@@ -2,8 +2,6 @@ from django.urls import path
 from medias import views
 
 urlpatterns = [
-    path(
-        "photos/<int:pk>",
-        views.PhotoDetail.as_view(),
-    )
+    path("photos/get-url", views.GetUploadUrl.as_view()),
+    path("photos/<int:pk>", views.PhotoDetail.as_view()),
 ]
--- airbnb_poetry/medias/views.py
+++ airbnb_poetry/medias/views.py
@@ -1,8 +1,10 @@
+from django.conf import settings
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.exceptions import NotFound, PermissionDenied
 from rest_framework.response import Response
 from rest_framework.status import HTTP_204_NO_CONTENT
+import requests
 from .models import Photo
 
 
@@ -24,3 +26,13 @@ class PhotoDetail(APIView):
         else:
             photo.delete()
             return Response(status=HTTP_204_NO_CONTENT)
+
+
+class GetUploadUrl(APIView):
+    def post(self, request):
+        url = f"https://api.cloudflare.com/client/v4/accounts/{settings.CF_ID}/images/v2/direct_upload"
+        one_time_url = requests.post(
+            url, headers={"Authorization": f"Bearer {settings.CF_TOKEN}"}
+        )
+        one_time_url = one_time_url.json().get("result")
+        return Response(one_time_url)
